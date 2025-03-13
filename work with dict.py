e85e8232f6cb7fbfe2e8b2dd32bad08865f999f7
DanilMamenko <makkos.181918@gmail.com>
2025-03-13 15:33:52 +0200

    Add files via upload

---
+++ work with dict.py
@@ -0,0 +1,15 @@
+dict_test = {'car': 'Toyota', 'price': 4900, 'where': 'EU'}
+print (dict_test['car'])
+print (dict_test['where'])
+
+print ('\nKeys and Values of dict')
+x = dict_test.keys()
+x1 = dict_test.values()
+print (x)
+print (x1)
+
+print('\nItems of dict')
+y = dict_test.items()
+print (y)
+
+
