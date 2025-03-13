fe392ddd9b3343d2302f072fe19bb909aca3cca3
DanilMamenko <makkos.181918@gmail.com>
2025-03-13 15:10:02 +0200

    Add files via upload

---
+++ work with str.py
@@ -0,0 +1,16 @@
+num_str = 125;
+str(num_str);
+print (num_str)
+
+message = 'Hi, my name is Python!'
+x = message.replace('y', '0')
+x1 = x.replace("i", "1")
+print (x1)
+
+split_test = 'This is a split test'
+y = split_test.split()
+print (y)
+string_join= " ".join(y)
+print(string_join)
+y1 = len(string_join)
+print (y1)
